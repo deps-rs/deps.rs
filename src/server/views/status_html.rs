ab48353ce8aad68c9072d1168a0c4a4c62310b82
Sam Rijs <srijs@airpost.net>
2018-01-29 10:01:01 +1100

    design: make viewport responsive

--- src/server/views/status_html.rs
+++ src/server/views/status_html.rs
@@ -85,6 +85,8 @@ pub fn status_html(analysis_outcome: AnalyzeDependenciesOutcome, repo_path: Repo
     let rendered = html! {
         html {
             head {
+                meta charset="utf-8";
+                meta name="viewport" content="width=device-width, initial-scale=1";
                 title (title)
                 link rel="stylesheet" type="text/css" href="/static/style.css";
             }
