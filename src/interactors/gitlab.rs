c5d53d7f2c585dacbcd74dc8acd4cce352d8adbd
Paul Woolcock <paul@woolcock.us>
2018-02-12 22:30:15 -0500

    change master -> HEAD

--- src/interactors/gitlab.rs
+++ src/interactors/gitlab.rs
@@ -8,14 +8,14 @@ const GITLAB_USER_CONTENT_BASE_URI: &'static str = "https://gitlab.com";
 
 pub fn get_manifest_uri(repo_path: &RepoPath, path: &RelativePathBuf) -> Result<Uri, Error> {
     let path_str: &str = path.as_ref();
-    // gitlab will return a 308 if the Uri ends with, say, `.../raw/master//Cargo.toml`, so make
+    // gitlab will return a 308 if the Uri ends with, say, `.../raw/HEAD//Cargo.toml`, so make
     // sure that last slash isn't doubled
     let slash_path = if path_str.starts_with("/") {
         &path_str[1..]
     } else {
         path_str
     };
-    Ok(format!("{}/{}/{}/raw/master/{}",
+    Ok(format!("{}/{}/{}/raw/HEAD/{}",
         GITLAB_USER_CONTENT_BASE_URI,
         repo_path.qual.as_ref(),
         repo_path.name.as_ref(),
