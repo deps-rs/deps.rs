161e29e6d6e640cdfb113302da45416e9b80a629
Paul Woolcock <paul@woolcock.us>
2018-02-12 22:32:54 -0500

    use failure::Error instead of hyper::error::UriError

--- src/interactors/bitbucket.rs
+++ src/interactors/bitbucket.rs
@@ -1,3 +1,4 @@
+use failure::Error;
 use hyper::Uri;
 use relative_path::RelativePathBuf;
 
@@ -5,13 +6,13 @@ use ::models::repo::RepoPath;
 
 const BITBUCKET_USER_CONTENT_BASE_URI: &'static str = "https://bitbucket.org";
 
-pub fn get_manifest_uri(repo_path: &RepoPath, path: &RelativePathBuf) -> Result<Uri, ::hyper::error::UriError> {
+pub fn get_manifest_uri(repo_path: &RepoPath, path: &RelativePathBuf) -> Result<Uri, Error> {
     let path_str: &str = path.as_ref();
-    format!("{}/{}/{}/raw/master/{}",
+    Ok(format!("{}/{}/{}/raw/master/{}",
         BITBUCKET_USER_CONTENT_BASE_URI,
         repo_path.qual.as_ref(),
         repo_path.name.as_ref(),
         path_str
-    ).parse::<Uri>()
+    ).parse::<Uri>()?)
 }
 
