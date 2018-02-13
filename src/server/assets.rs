4bdebcaaadb9edab92c5c6c027278bc523c8c884
Sam Rijs <srijs@airpost.net>
2018-02-14 08:28:13 +1100

    change color on up-to-date badge to a brighter green

--- src/server/assets.rs
+++ src/server/assets.rs
@@ -1,6 +1,6 @@
 //pub mod templates;
 
-pub static BADGE_UPTODATE_SVG: &'static [u8; 978] =
+pub static BADGE_UPTODATE_SVG: &'static [u8; 975] =
     include_bytes!("../../assets/badges/up-to-date.svg");
 pub static BADGE_OUTDATED_SVG: &'static [u8; 974] =
     include_bytes!("../../assets/badges/outdated.svg");
