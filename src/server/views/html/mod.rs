5d7e78d6cdba1989ea2eab9370656da8af05b885
Paolo Barbolini <paolo@paolo565.org>
2021-02-10 09:42:47 +0100

    Use font-awesome-as-a-crate instead of importing the entire Font Awesome

--- src/server/views/html/mod.rs
+++ src/server/views/html/mod.rs
@@ -21,7 +21,6 @@ fn render_html<B: Render>(title: &str, body: B) -> Response<Body> {
                 link rel="stylesheet" type="text/css" href="/static/style.css";
                 link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600";
                 link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Code+Pro";
-                link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
             }
             body { (body) }
         }
