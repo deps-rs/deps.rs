aa48e8af6d3c641f1a8edc13a45a84fa5bb5915e
Felix Wittwer <dev@felixwittwer.de>
2022-09-01 23:29:51 +0200

    Display sub-directory tree in status overview

--- src/server/views/html/status.rs
+++ src/server/views/html/status.rs
@@ -161,6 +161,23 @@ fn render_title(subject_path: &SubjectPath) -> Markup {
     }
 }
 
+/// Renders a path within a repository as HTML.
+///
+/// Panics, when the string is empty.
+fn render_path(inner_path: &str) -> Markup {
+    let path_icon = PreEscaped(fa(FaType::Regular, "folder-open").unwrap());
+
+    let mut splitted = inner_path.trim_matches('/').split('/');
+    let init = splitted.next().unwrap().to_string();
+    let path_spaced = splitted.fold(init, |b, val| b + " / " + val);
+
+    html! {
+        { (path_icon) }
+        " / "
+        (path_spaced)
+    }
+}
+
 fn dependencies_pluralized(count: usize) -> &'static str {
     if count == 1 {
         "dependency"
@@ -332,6 +349,7 @@ fn render_failure(subject_path: SubjectPath) -> Markup {
 fn render_success(
     analysis_outcome: AnalyzeDependenciesOutcome,
     subject_path: SubjectPath,
+    extra_config: ExtraConfig
 ) -> Markup {
     let self_path = match subject_path {
         SubjectPath::Repo(ref repo_path) => format!(
@@ -347,7 +365,7 @@ fn render_success(
     let status_base_url = format!("{}/{}", &super::SELF_BASE_URL as &str, self_path);
 
     let status_data_uri =
-        badge::badge(Some(&analysis_outcome), ExtraConfig::default()).to_svg_data_uri();
+        badge::badge(Some(&analysis_outcome), extra_config.clone()).to_svg_data_uri();
 
     let hero_class = if analysis_outcome.any_always_insecure() {
         "is-danger"
@@ -364,6 +382,12 @@ fn render_success(
                 div class="container" {
                     h1 class="title is-1" {
                         (render_title(&subject_path))
+                    }
+
+                    @if let Some(path) = extra_config.path {
+                        p class="subtitle" {
+                            (render_path(&path))
+                        }
                     }
 
                     img src=(status_data_uri);
@@ -416,6 +440,7 @@ fn render_success(
 pub fn render(
     analysis_outcome: Option<AnalyzeDependenciesOutcome>,
     subject_path: SubjectPath,
+    extra_config: ExtraConfig,
 ) -> Response<Body> {
     let title = match subject_path {
         SubjectPath::Repo(ref repo_path) => {
@@ -427,7 +452,7 @@ pub fn render(
     };
 
     if let Some(outcome) = analysis_outcome {
-        super::render_html(&title, render_success(outcome, subject_path))
+        super::render_html(&title, render_success(outcome, subject_path, extra_config))
     } else {
         super::render_html(&title, render_failure(subject_path))
     }
