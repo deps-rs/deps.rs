1f290b13359cb9f087cf3d6f047b576b344012d0
Paolo Barbolini <paolo.barbolini@m4ss.net>
2024-12-02 12:32:47 +0100

    refactor: drop unused cadence metrics

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 #![deny(rust_2018_idioms)]
 #![warn(missing_debug_implementations)]
 
-use std::{
-    env,
-    net::{Ipv4Addr, UdpSocket},
-    time::Duration,
-};
+use std::{env, net::Ipv4Addr, time::Duration};
 
 use actix_web::{
     middleware::Logger,
     web::{self, ThinData},
 };
 use actix_web_lab::middleware::NormalizePath;
-use cadence::{QueuingMetricSink, UdpMetricSink};
 use reqwest::redirect::Policy as RedirectPolicy;
 
 mod engine;
@@ -25,14 +20,6 @@ mod utils;
 use self::{engine::Engine, utils::index::ManagedIndex};
 
 const DEPS_RS_UA: &str = "deps.rs";
-
-fn init_metrics() -> QueuingMetricSink {
-    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
-    socket.set_nonblocking(true).unwrap();
-    let host = ("127.0.0.1", 8125);
-    let sink = UdpMetricSink::from(host, socket).unwrap();
-    QueuingMetricSink::from(sink)
-}
 
 fn init_tracing_subscriber() {
     use tracing::level_filters::LevelFilter;
@@ -57,7 +44,6 @@ fn init_tracing_subscriber() {
 async fn main() {
     dotenvy::dotenv().ok();
     init_tracing_subscriber();
-    let metrics = init_metrics();
 
     let client = reqwest::Client::builder()
         .user_agent(DEPS_RS_UA)
@@ -81,8 +67,7 @@ async fn main() {
         });
     }
 
-    let mut engine = Engine::new(client.clone(), index);
-    engine.set_metrics(metrics);
+    let engine = Engine::new(client.clone(), index);
 
     let server = actix_web::HttpServer::new(move || {
         actix_web::App::new()
